9a670b8cabcec0043104906725b722a418e0fe58
Apinop <apinop.soisuwan@gmail.com>
2021-12-13 02:26:45 +0700

    adding models and create superuser

--- pygo/myblog/models.py
+++ pygo/myblog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class community_post (models.Model):
+    first_bar = models.CharField(max_length=20)
+    # formal id
+    sec_bar = models.TextField(max_length=20)
+    # *** Password 
